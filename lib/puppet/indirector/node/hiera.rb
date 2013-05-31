ec3b0142d5a5d65ddb82d97fe4421193dc0e63c7
Thomas Linkin <trlinkin@gmail.com>
2013-05-31 14:29:08 -0400

    Code clean - prep for rewrite
    This commit cleans up tabbing for comments and removes whitespaces.

--- lib/puppet/indirector/node/hiera.rb
+++ lib/puppet/indirector/node/hiera.rb
@@ -6,7 +6,8 @@ require 'hiera'
 require 'hiera/scope'
 
 class Puppet::Node::Hiera < Puppet::Indirector::Plain
-  desc "Get node information from Hiera"
+
+  desc "Classify nodes in Hiera via discovered context derived from the Certificate Name"
   include Puppet::Util
 
   # Look for external node definitions.
@@ -20,7 +21,7 @@ class Puppet::Node::Hiera < Puppet::Indirector::Plain
 
   private
 
-# init Hiera
+  # init Hiera
   def setup_hiera()
     configfile = File.join([File.dirname(Puppet.settings[:config]), "hiera.yaml"])
 
@@ -33,46 +34,46 @@ class Puppet::Node::Hiera < Puppet::Indirector::Plain
     @hiera = Hiera.new(:config => config)
   end
 
-# pull in scoped Hiera results
+  # pull in scoped Hiera results
   def populate_node(key, node)
-    hscope = populate_scope(key, node)    
+    hscope = populate_scope(key, node)
 
-  # get classes
+    # get classes
     classes = @hiera.lookup('classes', nil, hscope, nil, :hash) || {}
 
-  # translate namespace resolution operators
+    # translate namespace resolution operators
     classes.each do |k,v|
       classes[k.gsub('.', '::')] = v
       classes.delete(k) if k =~ /\./
-    end    
+    end
 
-  # put the things Hiera discovered into our node
+    # put the things Hiera discovered into our node
     node.classes     = classes
     node.parameters  = @hiera.lookup('parameters', nil, hscope, nil, :hash) || {}
 
     # TODO: the Hiera lookup for this keeps failing.  debug it
     #node.environment = env if env
 
-  # merge new facts into existing facts
+    # merge new facts into existing facts
     node.fact_merge
     node
   end
 
-# create a new scope and populate it with facts pulled from the indirector
+  # create a new scope and populate it with facts pulled from the indirector
   def populate_scope(key, node)
-  # create a new scope because i'm not sure if/how any existing scope
-  # for this node can be retreived
+    # create a new scope because i'm not sure if/how any existing scope
+    # for this node can be retreived
     scope = Puppet::Parser::Scope.new
 
-  # get facts from REST API, or empty hash
+    # get facts from REST API, or empty hash
     facts = Puppet::Node::Facts.indirection.find(key).values rescue {}
 
-  # add node facts to our bootleg scope
+    # add node facts to our bootleg scope
     facts.each do |fact, value|
       scope.setvar(fact, value)
     end
 
-  # return the Hierafied scope
+    # return the Hierafied scope
     Hiera::Scope.new(scope)
   end
 end
